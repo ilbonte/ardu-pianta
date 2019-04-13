4f1b40bcdf2f78e60c2d371a7471ce501175de07
Davide Bontempelli <davide.bonte@gmail.com>
2019-04-13 18:10:00 +0200

    Create google-script.js

---
+++ google-script.js
@@ -0,0 +1,37 @@
+function doPost(e) {
+  var rawData = e.postData.contents;
+  var cloudData = JSON.parse(rawData);
+
+  var data = {};
+
+  for (var i = 0; i < cloudData.values.length; i++) {
+    data[cloudData.values[i].name] = cloudData.values[i].value;
+  }
+
+  console.log(data);
+
+
+  var message = ""
+  if(data.temperatura || data.moisture || data.luce){
+    if(parseInt(data.temperatura) > (35))
+      message = "che caldo!"
+      
+     if(parseInt(data.luce) > 600)
+      message = "che luce! ☀️"
+      
+     if(parseInt(data.moisture) < 240)
+      message = "che sete!"
+  }
+  
+    var url =
+    "https://maker.ifttt.com/trigger/provaPianta/with/key/kywqnZg1-5Pq7AxqTnf56VmynZOXg54lQTOZKu6xAEj";
+  var options = {
+    method: "post",
+    payload: {
+      value1: message
+    }
+  };
+
+  if(data.shouldUpdate === true)
+    UrlFetchApp.fetch(url, options);
+}
